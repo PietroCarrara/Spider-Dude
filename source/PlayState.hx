f56262cadaa0faef7f8365fecea4f533eff6414a
Pietro Carrara <piticarrara@gmail.com>
2019-01-07 18:37:23 -0200

    Ugly jump

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,8 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxSprite;
 import entities.Character;
 
 class PlayState extends FlxState {
@@ -12,6 +14,13 @@ class PlayState extends FlxState {
 		char = new Character();
 		this.add(char);
 		this.camera.follow(char);
+
+		var sqr = new FlxSprite();
+		sqr.x = 200;
+		sqr.makeGraphic(200, 200);
+		this.add(sqr);
+
+		this.camera.follow(sqr);
 	}
 
 	override public function update(elapsed:Float):Void {
--- source/entities/Character.hx
+++ source/entities/Character.hx
@@ -1,9 +1,11 @@
 package entities;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.math.FlxPoint;
 
 class Character extends FlxSprite {
+	private var maxJumpForce = 600;
 
 	function new(?x:Float, ?y:Float) {
 		super(x, y);
@@ -14,5 +16,29 @@ class Character extends FlxSprite {
 
 	override function update(dt:Float) {
 		super.update(dt);
+
+		if (FlxG.swipes.length > 0) {
+			var swipe = FlxG.swipes[0];
+
+			// Screen point to world point
+			swipe.endPosition.x += this.x - FlxG.width / 2;
+			swipe.endPosition.y += this.y - FlxG.height / 2;
+
+			var angle = flixel.math.FlxAngle.angleBetweenPoint(this, swipe.endPosition);
+
+			trace("angle", angle);
+			trace("cos", Math.cos(angle));
+			trace("sin", Math.sin(angle));
+
+			// The touch was below the character, so its ok
+			if (angle > 0) {
+				var force = Math.min(swipe.distance, maxJumpForce);
+
+				this.velocity.x = -Math.cos(angle) * force;
+				this.velocity.y = -Math.sin(angle) * force;
+
+				this.acceleration.y = maxJumpForce / 2;
+			}
+		}
 	}
 }
